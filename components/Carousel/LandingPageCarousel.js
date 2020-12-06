c43e4595144556af7226307fefc322b5112194a8
s3539822 <41284457+s3539822@users.noreply.github.com>
2020-12-06 16:59:43 +1100

    Update LandingPageCarousel.js

--- components/Carousel/LandingPageCarousel.js
+++ components/Carousel/LandingPageCarousel.js
@@ -18,7 +18,7 @@ export default function LandingPageCarousel(props) {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false
+        autoplay: true
     };
 
     function Slide(image, header, body, buttonText) {

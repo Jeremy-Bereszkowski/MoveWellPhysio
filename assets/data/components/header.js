07f3ba19d213d98ee21250698ea33189550fff4e
s3539822 <41284457+s3539822@users.noreply.github.com>
2020-12-31 18:05:55 +1100

    Update to Cliniko

--- assets/data/components/header.js
+++ assets/data/components/header.js
@@ -58,13 +58,13 @@ const HeaderData = {
                 {
                     core: {
                         key: "Consults",
-                        href: "https://movewell-physiotherapy-melbourne.au2.cliniko.com/bookings"
+                        href: URL.CLINIKO,
                     },
                 },
                 {
                     core: {
                         key: "Group Sessions",
-                        href: URL.MIND_BODY,
+                        href: URL.CLINIKO,
                     },
                 },
             ],

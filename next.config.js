d67b27caf77198aeadeca94540a6c7cd7bf13b0b
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-03-13 12:28:30 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,24 +8,5 @@ module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
-  }/*,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://www.instagram.com/movewellmelbourne/',
-      },
-    ]
-  },*/
+  }
 });
-
-/*module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
-      },
-    ]
-  },
-};*/
